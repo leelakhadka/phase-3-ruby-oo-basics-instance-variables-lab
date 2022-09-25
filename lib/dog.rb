093e4d37a6ac60226c017acd5b807db4587af811
leela khadka <khadkaleela3@gmail.com>
2022-09-24 19:10:15 -0700

    tests passed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
 
+ #setter method to set the dog name   
+def name=(dog_name)
+    @this_dogs_name = dog_name
+end
+
+#getter method to get the dog name
+def name
+    @this_dogs_name
+end
 end
